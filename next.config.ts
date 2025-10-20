fac823c83dcfdf80b35197fa742a3307ba743fa6
Farzana <farzana@wevics.com>
2025-10-20 11:38:43 +0600

    next.config.ts changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   experimental: {
+    serverActions: {
+      bodySizeLimit: '1gb', // ✅ increase from default 1MB to 1GB
+    },
+  },
 };
 
 export default nextConfig;
